297510bed5c79873263efce708cd6b656092c3f0
asddsaf <convoo@gmail.com>
2015-10-03 15:06:04 +0200

    API key + egy próba lekérdezés

--- MI/src/Main.java
+++ MI/src/Main.java
@@ -1,9 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+
+
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("asd");
-	}
+	public static final String APIKEY = "7d7d86e0683f91595c5d6784f12da0c5";
 
+	public static void main(String[] args) {
+
+	try {
+			URL url = new URL("http://ws.audioscrobbler.com/2.0/?method=artist.gettoptags&artist=vad%20fruttik&api_key="+APIKEY);
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					url.openStream()));
+			String strTemp = "";
+			while (null != (strTemp = br.readLine())) {
+				System.out.println(strTemp);
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+
+
+
+	
+}
 }
