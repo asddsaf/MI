5576548eb0688b4b490b491935c0eb43d158b088
nikoletk <nikolett37@gmail.com>
2015-10-06 21:28:00 +0200

    Valid XML file is created

--- MI/src/Main.java
+++ MI/src/Main.java
@@ -1,4 +1,6 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.net.URL;
 
@@ -11,21 +13,42 @@ public class Main {
 
 	public static void main(String[] args) {
 
-	try {
-			URL url = new URL("http://ws.audioscrobbler.com/2.0/?method=artist.gettoptags&artist=vad%20fruttik&api_key="+APIKEY);
-			BufferedReader br = new BufferedReader(new InputStreamReader(
-					url.openStream()));
+		try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter("data.xml"));
+			//Get top artists of nikolettk
+			URL url = new URL("http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=nikolettk&limit=10&api_key="+APIKEY);
+			BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
 			String strTemp = "";
+			bw.write("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>");
+			bw.write("<root>");
 			while (null != (strTemp = br.readLine())) {
-				System.out.println(strTemp);
+				int startIndex = strTemp.indexOf("<name>");
+				if ( startIndex != -1) {
+					int endIndex = strTemp.indexOf("</name>");
+					String artist = strTemp.substring(startIndex+6, endIndex);
+					URL artistUrl = new URL("http://ws.audioscrobbler.com/2.0/?method=artist.gettoptags&artist=" + artist + "&api_key="+APIKEY);					
+					BufferedReader artistBr = new BufferedReader(new InputStreamReader(artistUrl.openStream()));
+					String artistTemp = "";
+					while ((artistTemp = artistBr.readLine())!= null) {
+						if (artistTemp.indexOf("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>") != -1) {
+							artistTemp = artistBr.readLine();
+						}
+						if ((artistTemp.indexOf("<lfm") != -1)) {
+							artistTemp = artistTemp.replace("<lfm status=\"ok\">", "");
+						}
+						if ((artistTemp.indexOf("</lfm") != -1)) {
+							artistTemp = artistTemp.replace("</lfm>", "");
+						}
+						System.out.println(artistTemp);
+						bw.write(artistTemp);
+					}
+				}
 			}
+			bw.write("</root>");
+			bw.close();
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-
-
-
-
-	
-}
+		
+	}
 }
