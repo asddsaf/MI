bcbb894153ea20ada29069e69afebdb848be2044
nikoletk <nikolett37@gmail.com>
2015-11-22 00:06:07 +0100

    K-means algoritmus kezdetleges implementálása, még hiányzik: changed változó állítása, pont kivétele az aktuális klaszterből, ha egy másikba átkerül, tesztelni, hogy működik-e (és jó-e)

--- MI/src/Cluster.java
+++ MI/src/Cluster.java
@@ -27,5 +27,25 @@ public class Cluster {
 		this.centroid = centroid;
 	}
 	
+	public int[] calculateCentroid() {
+		//majority vote alapján
+		int voteVector[] = new int[LastFMHandler.MAXARTISTS]; //ezeknél nem biztos, hogy jó a maxartists, mert nincs annyi
+		int resultVector[] = new int[LastFMHandler.MAXARTISTS];
+		for (int j = 0; j<LastFMHandler.MAXARTISTS; j++) {
+			for (int i = 0; i<points.size(); i++) {
+				int[] artists = points.get(i).getArtists();
+				voteVector[j] += artists[j];
+			}
+			if (voteVector[j] > points.size()/2) { //ha több, mint a felénél az adott bit 1, akkor 1 a result vectorban is (majority)
+				resultVector[j] = 1;
+			}
+		}
+		return resultVector;
+	}
+	
+	public Point getCentroid() {
+		return centroid;
+	}
+	
 	
 }
--- MI/src/KMeans.java
+++ MI/src/KMeans.java
@@ -13,8 +13,43 @@ public class KMeans {
 		points = DataProcessor.getPoints();
 	}
 	
-	public void createClusters(int k){
+	//kell k: hány klasztert akarunk, és kellenek a kezdõ centroidok (k db)
+	//végén: az kell, hogy az egyes klaszterekben mely pontok vannak együtt
+	public void createClusters(int k, Point[] initialCentroids){
+		//létrehozunk k üres klasztert
+		for (int i = 0; i<k; i++) {
+			clusters.add(new Cluster(new ArrayList<Point>(), initialCentroids[i]));
+		}
 		
+		boolean changed = true; //addig megyünk, amíg elõfordul olyan, hogy 1 pontot másik klaszterbe sorolunk
+		
+		while(changed) {
+			
+			for (int i = 0; i<points.size(); i++) { //végigmegyünk az összes ponton
+				
+				int distance = 150; //ez béna, de egyelõre megteszi
+				int clusterNum = 0;
+				
+				for (int j = 0; j<k; j++) { //és megkeressük, melyik klaszterhez van közelebb
+					int tempDistance = Point.distance(points.get(i), clusters.get(j).getCentroid());
+					if (tempDistance < distance) {
+						distance = tempDistance;
+						clusterNum = j;
+					}
+				}
+				
+				//TODO: i. pont kivétele az aktuális klaszterébõl, ha az más, mint a most megtalált
+				//TODO: changed változót állítani
+				
+				//megtaláltuk az i. ponthoz tartozó legközelebbi klasztert (clusterNum), ebbe soroljuk
+				clusters.get(clusterNum).addPoint(points.get(i));
+			}
+			//miután minden pontot besoroltunk a megfelelõ klaszterbe, kiszámítjuk a klaszterek új centroidját
+			for (int i = 0; i<k; i++) {
+				Point newCentroid = new Point("centroid", clusters.get(i).calculateCentroid());
+				clusters.get(i).setCentroid(newCentroid);
+			}
+		}
 	}
 	
 	
